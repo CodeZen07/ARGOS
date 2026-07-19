92e14e9e2405f634606ee2f5a45bdae4e9ce1272
CodeZen07 <ing.juancarlostejeda@gmail.com>
2026-07-19 08:33:11 -0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,26 @@
+from fastapi import FastAPI
+from database import engine
+from models import Base, Licitacion
+from scraping import scrape_portal
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "API ARGOS funcionando 🚀"}
+
+@app.get("/licitaciones")
+def get_licitaciones():
+    from database import SessionLocal
+    db = SessionLocal()
+    data = db.query(Licitacion).all()
+    db.close()
+    return data
+
+@app.post("/actualizar")
+def actualizar():
+    scrape_portal()
+    return {"message": "Datos actualizados"}
 
